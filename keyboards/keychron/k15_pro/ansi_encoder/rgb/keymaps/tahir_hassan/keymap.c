c96d9447d6704e7abda55f5a412f3d60aa58d077
Tahir Hassan <tahir.a.hassan@gmail.com>
2024-06-09 21:53:15 +0100

    backspace

--- keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
+++ keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
@@ -43,20 +43,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,  _______,            _______,  _______,  _______,             _______,            _______,            _______,             _______,  _______,  _______),
 
     [TAHIR_BASE] = LAYOUT_90_ansi(
-        KC_MUTE,    KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,      KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,                       KC_DEL,
-        MC_1,       KC_GRV,             KC_1,     KC_2,     KC_3,     KC_4,       KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,                      KC_PGUP,
-        MC_2,       KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                      KC_PGDN,
-        MC_3,       MO(TAHIR_UDLR),     KC_A,     KC_S,     KC_D,     KC_F,       KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,                       KC_HOME,
-        MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,       KC_B,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   MO(WIN_FN),            KC_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MUTE,    KC_ESC,             KC_F1,    KC_F2,    KC_F3,         KC_F4,      KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,                       KC_DEL,
+        MC_1,       KC_GRV,             KC_1,     KC_2,     KC_3,          KC_4,       KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,                      KC_PGUP,
+        MC_2,       KC_TAB,             KC_Q,     KC_W,     KC_E,          KC_R,       KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                      KC_PGDN,
+        MC_3,       MO(TAHIR_UDLR),     KC_A,     KC_S,     KC_D,          KC_F,       KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,                       KC_HOME,
+        MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,          KC_V,       KC_B,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_BACKSPACE,  MO(WIN_FN),            KC_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [TAHIR_UDLR] = LAYOUT_90_ansi(
-        KC_MUTE,    KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
-        MC_1,       KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
-        MC_2,       KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
-        MC_3,       KC_NO,              KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
-        MC_4,       KC_LSFT,            KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_RSFT,  KC_UP,
-        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   MO(WIN_FN),            KC_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MUTE,    KC_NO,              KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
+        MC_1,       KC_NO,              KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
+        MC_2,       KC_NO,              KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
+        MC_3,       KC_NO,              KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,
+        MC_4,       KC_LSFT,            KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_RSFT,  KC_UP,
+        MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_BACKSPACE,  MO(WIN_FN),            KC_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
