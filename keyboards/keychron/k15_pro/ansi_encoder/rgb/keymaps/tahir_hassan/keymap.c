0e393c06f302b65ed1ca9258e04f9336eec02f62
Tahir Hassan <tahir.a.hassan@gmail.com>
2024-07-25 00:41:24 +0100

    added home and end in the right place (on the right of the keyboard). Added comma and dot do home and end in the UDLR keymap

--- keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
+++ keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [TAHIR_BASE] = LAYOUT_90_ansi(
         KC_MUTE,   /*_*/     KC_ESC,          KC_F1,    KC_F2,    KC_F3,        KC_F4,       KC_F5,   /**/    KC_F6,      /*|*/      KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,    MO(TAHIR_FN),
         MC_1,      /*_*/     KC_GRV,          KC_1,     KC_2,     KC_3,         KC_4,        KC_5,    /**/    KC_6,       /*|*/      KC_7,     KC_8,      KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_PGUP,
-        MC_2,      /*_*/     KC_TAB,          KC_Q,     KC_W,     KC_E,         KC_R,        KC_T,    /*|*/   /**/        KC_Y,      KC_U,     KC_I,      KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGDN,
-        MC_3,      /*_*/     MO(TAHIR_UDLR),  KC_A,     KC_S,     KC_D,         KC_F,        KC_G,    /*|*/   /**/        KC_H,      KC_J,     KC_K,      KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   /*_*/      KC_HOME,
+        MC_2,      /*_*/     KC_TAB,          KC_Q,     KC_W,     KC_E,         KC_R,        KC_T,    /*|*/   /**/        KC_Y,      KC_U,     KC_I,      KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_HOME,
+        MC_3,      /*_*/     MO(TAHIR_UDLR),  KC_A,     KC_S,     KC_D,         KC_F,        KC_G,    /*|*/   /**/        KC_H,      KC_J,     KC_K,      KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   /*_*/      KC_END,
         MC_4,      /*_*/     TD(TD_LSFT_ESC), KC_Z,     KC_X,     KC_C,         KC_V,        KC_B,    /*|*/   KC_B,       KC_N,      KC_M,     KC_COMMA,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    /*_*/
         MC_5,      KC_LCTL,  KC_LWIN,         /*_*/     KC_LALT,  KC_BSPC,      /*>*/        KC_DEL,  /*|*/   KC_SPACE,   /*>*/      KC_RALT,  KC_RCTL,   /*_*/     /*_*/     KC_LEFT,  KC_DOWN,   KC_RGHT ),
 
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_1,     /*_*/      KC_NO,           KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,   /**/    KC_NO,      /*|*/      KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,
         MC_2,     /*_*/      KC_NO,           KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,   /*|*/   /**/        KC_NO,     KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,
         MC_3,     /*_*/      KC_NO,           KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,   /*|*/   /**/        KC_LEFT,   KC_DOWN,  KC_UP,     KC_RIGHT, KC_NO,    KC_NO,    KC_NO,    /*_*/      KC_NO,
-        MC_4,     /*_*/      KC_LSFT,         KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,   /*|*/   KC_NO,      KC_NO,     KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_RSFT,  KC_UP,    /*_*/
+        MC_4,     /*_*/      KC_LSFT,         KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,   /*|*/   KC_NO,      KC_NO,     KC_NO,    KC_HOME,   KC_END,   KC_NO,    KC_RSFT,  KC_UP,    /*_*/
         MC_5,     KC_LCTL,   KC_LWIN,         /*_*/     KC_LALT,  KC_BACKSPACE,  /*>*/       KC_NO,   /*|*/   KC_SPC,     /*>*/      KC_RALT,  KC_RCTL,   /*_*/     /*_*/     KC_LEFT,  KC_DOWN,  KC_RGHT),
 };
 
@@ -128,6 +128,8 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         for (int i = start; i <= end; ++i) {
             rgb_matrix_set_color(i, RGB_WHITE);
         }
+        rgb_matrix_set_color(72, RGB_WHITE); // KC_COMMA
+        rgb_matrix_set_color(73, RGB_WHITE); // KC_DOT
     }
 
     return false;
