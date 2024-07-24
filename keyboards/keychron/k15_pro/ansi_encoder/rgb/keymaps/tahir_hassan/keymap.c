c6b48908870ddb61078e2c93c32e77281d5be1e4
Tahir Hassan <tahir.a.hassan@gmail.com>
2024-07-25 00:12:32 +0100

    enabled tap dance for making shift do escape on double tap.  Increased the autoshift timeout to 200, and further still for command and period

--- keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
+++ keyboards/keychron/k15_pro/ansi_encoder/rgb/keymaps/tahir_hassan/keymap.c
@@ -20,6 +20,8 @@
 #include "encoder.h"
 #include "rgb_matrix.h"
 #include "keycodes.h"
+#include "process_auto_shift.h"
+#include "process_tap_dance.h"
 
 #include QMK_KEYBOARD_H
 
@@ -30,6 +32,18 @@ enum layers{
     TAHIR_FN,
     TAHIR_UDLR
 };
+
+// Tap Dance declarations
+enum {
+    TD_LSFT_ESC,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_LSFT_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_ESC),
+};
+
 
 #ifndef LAYOUT_90_ansi
 #define LAYOUT_90_ansi(...) { __VA_ARGS__ }
@@ -74,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_1,      /*_*/     KC_GRV,          KC_1,     KC_2,     KC_3,         KC_4,        KC_5,    /**/    KC_6,       /*|*/      KC_7,     KC_8,      KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,   KC_PGUP,
         MC_2,      /*_*/     KC_TAB,          KC_Q,     KC_W,     KC_E,         KC_R,        KC_T,    /*|*/   /**/        KC_Y,      KC_U,     KC_I,      KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGDN,
         MC_3,      /*_*/     MO(TAHIR_UDLR),  KC_A,     KC_S,     KC_D,         KC_F,        KC_G,    /*|*/   /**/        KC_H,      KC_J,     KC_K,      KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   /*_*/      KC_HOME,
-        MC_4,      /*_*/     KC_LSFT,         KC_Z,     KC_X,     KC_C,         KC_V,        KC_B,    /*|*/   KC_B,       KC_N,      KC_M,     KC_COMMA,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    /*_*/
+        MC_4,      /*_*/     TD(TD_LSFT_ESC), KC_Z,     KC_X,     KC_C,         KC_V,        KC_B,    /*|*/   KC_B,       KC_N,      KC_M,     KC_COMMA,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    /*_*/
         MC_5,      KC_LCTL,  KC_LWIN,         /*_*/     KC_LALT,  KC_BSPC,      /*>*/        KC_DEL,  /*|*/   KC_SPACE,   /*>*/      KC_RALT,  KC_RCTL,   /*_*/     /*_*/     KC_LEFT,  KC_DOWN,   KC_RGHT ),
 
 // if you hold down shift while pressing FN+RGB_MODE it goes in reverse order.
@@ -117,4 +131,15 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 
     return false;
+}
+
+uint16_t get_autoshift_timeout(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case KC_COMM:
+        case KC_DOT:
+            return get_generic_autoshift_timeout() + 75;
+        default:
+            return get_generic_autoshift_timeout();
+
+    }
 }
